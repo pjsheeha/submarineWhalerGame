4522920ac5d183aa98aec44c863ac31d7727c9dd
sheenufilms <yousheenu@gmail.com>
2020-06-19 16:44:56 -0700

    Animal Movement Behavior
    Added movement behavior and value code for photography

---
+++ SubmarineGame/Assets/planktoncode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planktoncode : MonoBehaviour
+{
+ 
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag == "whale")
+        {
+            if(GameObject.Find("Whale").GetComponent<FollowBehavior>().behavior == 1)
+            {
+                //Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ SubmarineGame/Assets/targetcode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetcode : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag == "whale")
+        {
+            if(GameObject.Find("Whale").GetComponent<FollowBehavior>().behavior == 2)
+            {
+                //Destroy(gameObject);
+            }
+        }
+        else if(coll.gameObject.tag == "Plankton")
+        {
+           
+             //Destroy(gameObject);
+
+        }
+    }
+}
