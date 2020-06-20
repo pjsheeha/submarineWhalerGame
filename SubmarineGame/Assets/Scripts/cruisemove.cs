550a0205500cfb8ab77639592fe87d3b72667ae0
Perebite <thebitegames@gmail.com>
2020-06-19 23:12:51 -0400

    check
    im trying to merge

---
+++ SubmarineGame/Assets/Scripts/cruisemove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cruisemove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    void Start()
+    {
+        speed = 0.95f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Vector3 position = this.transform.position;
+        this.transform.position += -transform.right * speed * Time.deltaTime;
+    }
+}
---
+++ SubmarineGame/Assets/unity-movement-ai-master/Assets/UnityMovementAI/Scripts/Units/PursueUnit.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace UnityMovementAI
+{
+
+    public class PursueUnit : MonoBehaviour
+    {
+        public MovementAIRigidbody target;
+        public float range = 11f;
+        public float iwanttoeat; 
+
+        SteeringBasics steeringBasics;
+        Pursue pursue;
+
+        void Start()
+        {
+            steeringBasics = GetComponent<SteeringBasics>();
+            pursue = GetComponent<Pursue>();
+            //CHANGE
+            InvokeRepeating("UpdateTarget", 0f, 0.5f);
+        }
+        //CHANGE
+        void UpdateTarget()
+        {
+
+            GameObject[] enemies2 = GameObject.FindGameObjectsWithTag("Target");
+            float shortestDistance = Mathf.Infinity;
+            GameObject nearestEnemy2 = null;
+
+            //FIND ALL TARGETS AND DISTANCE
+            foreach (GameObject enem in enemies2)
+            {
+                float distanceToEnemy = Vector3.Distance(transform.position, enem.transform.position);
+                if(distanceToEnemy < shortestDistance)
+                {
+                    shortestDistance = distanceToEnemy;
+                    nearestEnemy2 = enem;
+                }
+            }
+            //SEEK TARGETS
+            if (nearestEnemy2 != null && shortestDistance <= range)
+            {
+                target = nearestEnemy2.GetComponent<MovementAIRigidbody>();
+
+            } else if (nearestEnemy2 == null)
+            {
+                target = this.GetComponent<MovementAIRigidbody>();
+            }
+        }
+
+        void FixedUpdate()
+        {
+            Vector3 accel = pursue.GetSteering(target);
+            
+
+            steeringBasics.Steer(accel);
+            steeringBasics.LookWhereYoureGoing();
+        }
+    }
+}
