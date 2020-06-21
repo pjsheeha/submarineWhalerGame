c4ddc0829b7ffd12c4be896ca27535f5bfae9b1e
sheenufilms <yousheenu@gmail.com>
2020-06-21 02:55:08 -0700

    Fixed sprite issues and added value calculator to all animals

--- SubmarineGame/Assets/Scripts/valuecalculator.cs
+++ SubmarineGame/Assets/Scripts/valuecalculator.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class valuecalculator : MonoBehaviour
 {
     public float value;
+    public float fixedvalue;
     private float movementvalue;
+    public bool doesflips;
 
     void Start()
     {
@@ -15,7 +17,12 @@ public class valuecalculator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movementvalue = this.transform.rotation.eulerAngles.z;
-        value = Mathf.Abs(movementvalue -90);
+        if(doesflips == true)
+        {
+            movementvalue = this.transform.rotation.eulerAngles.z;
+            value = Mathf.Abs(movementvalue - fixedvalue);
+        } else {
+            value = fixedvalue;
+        }
     }
 }
